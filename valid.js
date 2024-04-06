77178bd57ef323044deffb2cce415fded354b671
VariableReThrushRate <bgpilot16@gmail.com>
2024-04-06 12:47:34 -0700

    Implimented basic creation of array for sending to PHP Ajax system

--- valid.js
+++ valid.js
@@ -18,3 +18,35 @@ else
 {
     alert("String length is: " + length + "\nSorry")
 }}
+function formacc(form) {
+    var name = form.querySelector('#buildName').value;
+    var address = form.querySelector('#buildAdd').value;
+    var latitude = form.querySelector('#buildLat').value;
+    var longitude = form.querySelector('#buildLong').value;
+    if (name == ''){
+        window.alert("Building name input is invalid! \n Please check it.")
+        return;
+    }
+    if (address == ''){
+        window.alert("Building Address input is invalid! \n Please check it.")
+        return;
+    }
+    if (latitude == ''){
+        window.alert("Building Latitude input is invalid! \n Please check it.")
+        return;
+    }
+    if (longitude == ''){
+        window.alert("Building Longitude! input is invalid! \n Please check it.")
+        return;
+    }
+    building = [name, address, latitude, longitude]
+}
+//Needs to be updated to function with our code, but basic GET request
+function callins(building){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+    document.getElementById("demo").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+}   
