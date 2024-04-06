0017ae37aa857a0e13c1a55a2614c4703e0930ed
VariableReThrushRate <bgpilot16@gmail.com>
2024-04-06 13:25:04 -0700

    Removed redundant template function

--- valid.js
+++ valid.js
@@ -1,23 +1,3 @@
-const stmartin =  /^[^\s@]+@stmartin\.edu$/i;
-function smucheck(form) {
-var inputElement = form.querySelector('#string');
-check = inputElement.value;;
-if (check == null)
-{
-    alert("sorry");
-    return;
-}
-length = check.length;
-console.log(stmartin.test(check));
-console.log(check);
-if (stmartin.test(check))
-{
-    alert("String length is: " + length + "\nThank you!")
-}
-else
-{
-    alert("String length is: " + length + "\nSorry")
-}}
 function formacc(form) {
     var text = new RegExp("/^\S.*(?:\r?\n\S.*)*$/u");
     var vname = new RegExp("[A-Za-z0-9'\.\-\s\,]");
