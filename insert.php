c603a94fd203084d13417ae6eee44c94c8a757b4
VariableReThrushRate <bgpilot16@gmail.com>
2024-04-11 15:16:41 -0700

    Began work on Insert function

---
+++ insert.php
@@ -0,0 +1,17 @@
+<?php
+function OpenCon()
+{
+$dbhost = "localhost";
+$dbuser = "viewer";
+$dbpass = "\$MUs8ints1";
+$db = "LocationDB";
+$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+return $conn;
+}
+$name <- "placeholder";
+$add <- "placeholder";
+$long <- "placeholder";
+$lat <- "placeholder";
+$purpose <- "placeholder";
+$conn <- OpenCon();
+$conn <- query("INSERT INTO building VALUES ($name), ($add), ($long), ($lat), ($purpose)");
