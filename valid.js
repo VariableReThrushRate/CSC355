3ea929a1375eb3c744d549cf49b1ed559616c563
VariableReThrushRate <bgpilot16@gmail.com>
2024-04-13 12:57:03 -0700

    Making initial changes to build out AJAX

--- valid.js
+++ valid.js
@@ -1,4 +1,5 @@
 function formacc(form) {
+    var oldform = form;
     var text = new RegExp("/^\S.*(?:\r?\n\S.*)*$/u");
     var vname = new RegExp("[A-Za-z0-9'\.\-\s\,]");
     var latlong = new RegExp("^-?([1-8]?[1-9]|[1-9]0)\.{1}\d{1,9}");
@@ -10,25 +11,26 @@ function formacc(form) {
     var buildpurp = form.querySelector('#buildPurp').value;
     if (name == '' || !vname.test(name) || checklen(name)){
         window.alert("Building name input is invalid! \n Please check it.")
-        return;
+        return null;
     }
     if (address == '' || !addr.test(address) || checklen(address)){
         window.alert("Building Address input is invalid! \n Please check it.")
-        return;
+        return null;
     }
     if (latitude == '' || !latlong.test(latitude)){
         window.alert("Building Latitude input is invalid! \n Please check it.")
-        return;
+        return null;
     }
     if (longitude == '' || !latlong.test(longitude)){
         window.alert("Building Longitude! input is invalid! \n Please check it.")
-        return;
+        return null;
     }
     if (buildpurp == '' || !text.test(buildpurp) || checklen(buildpurp)){
         window.alert("Building Purpose input is invalid! \n Please check it.")
-        return;
+        return null;
     }
-    building = [name, address, latitude, longitude, buildpurp]
+    //building = [name, address, latitude, longitude, buildpurp]
+    return oldform;
     //needs to be completed with AJAX call to work with unimplimented PHP insert function.
 }
 //Needs to be updated to function with our code, but basic GET request
