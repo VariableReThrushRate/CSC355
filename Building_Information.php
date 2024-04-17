d83f2f3434c66e983d09a6e8b8aea3f81a2cf140
VariableReThrushRate <bgpilot16@gmail.com>
2024-04-17 11:10:42 -0700

    Began implimenting PHP Dynamic Generation

--- Building_Information.php
+++ Building_Information.php
@@ -14,6 +14,7 @@
   $db = "LocationDB";
   $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
   return $conn;
+  
   }
   $conn <- OpenCon();
   $sid = $_GET['id'];
