514945ff8dc895c19f852932cc2d7f07e3f74da7
VariableReThrushRate <bgpilot16@gmail.com>
2024-04-17 17:36:25 -0700

    Got maps working

--- buildinfo.php
+++ buildinfo.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
-  <head><title>CSC355 Project Details Page</title></head>
+  <head><title>CSC355 Project Details Page</title>
   <link type="text/css" rel="stylesheet" href="style.css"/>
   <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-  <script type="module" src="mapdex.js"></script>
-
+  <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyDJ1hTb9kNjdiqVJUFmyp4dVEczkOr-49I", v: "weekly"});</script>
   <style>
     #map {height: 500px; width: auto;}
 
   </style>
-
+</head>
   
   <?php 
   $dbhost = "localhost";
@@ -43,10 +43,21 @@
             </tr>
         </tbody>
     </table>
+    <br>
     <script>
+      let map;
       // Initialize and add the map
-      (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: "AIzaSyDJ1hTb9kNjdiqVJUFmyp4dVEczkOr-49I", v: "weekly"});
+      
+        async function initMap() {
+        const { Map } = await google.maps.importLibrary("maps");
+
+        map = new Map(document.getElementById("map"), {
+        center: { lat: <?php echo($row['latitude'])?>, lng: <?php echo($row['longitude'])?> },
+        zoom: 18,
+      });
+    }
+    
+    initMap();
 
     </script>
 
